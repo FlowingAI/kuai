65600b18c3855a75e666729969053f4398285a0a
flowingai <flowingai@users.noreply.github.com>
2026-01-16 22:51:03 +0800

    修改 GitHub Pages 部署路径为 /kuai/

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   // GitHub Pages 部署路径
-  // 如果仓库名是 flowingai，base 设为 '/flowingai/'
+  // 仓库名是 kuai，base 设为 '/kuai/'
   // 如果使用自定义域名或用户/组织页面，base 设为 '/'
-  base: '/flowingai/',
+  base: '/kuai/',
   build: {
     outDir: 'dist',
     rollupOptions: {
